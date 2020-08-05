0f59d9ee831a50f071b9d2269a35e01ea4e1fb8e
Anna Morozova <anna.mik.morozova@gmail.com>
2020-08-04 23:21:50 -0400

    Feat: isRotation function

---
+++ ArraysAndStrings/isRotation/solution1.js
@@ -0,0 +1,17 @@
+// is str1 a substring of another str2
+// substring - a rotation of another word
+
+// Time complexity Big O(N);
+// Space complexity Big O(N);
+function isRotation(str1, str2) {
+    if (str1.length != str2.length) {
+        return false;
+    }
+    if ((str2 + str2).includes(str1)) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isRotation("waterbottle", "erbottlewat")); // true
+console.log(isRotation("iloveyou", "loveiyou")); //false

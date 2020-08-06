cbd292d9df926e058a8b7ce37140fff2b4068167
Kate Norton <katernorton@outlook.com>
2020-08-06 17:08:14 -0400

    bug-fix: solution 2 fixed based upon feedback

--- ArraysAndStrings/isPalinPerm/solution2.js
+++ ArraysAndStrings/isPalinPerm/solution2.js
@@ -1,4 +1,5 @@
 //function to check if there is a a palendrome in any permutation of string
+//ignore non-alpha char and spaces
 
 let yesP = 'qwertytrewq'
 let yesP2 = 'aabb'
@@ -6,6 +7,8 @@ let emptyStr = ''
 let noP = 'qre'
 let space = 'tacca to'
 let ignoreNonLet = 'ta#$(3784cotac'
+let tooManyRepeats = 'aaac'
+let tooManyRepeatsTrue = 'aaccc'
 
 //time O(n), space O(n)
 function palendromePermutation(str) {
@@ -13,10 +16,20 @@ function palendromePermutation(str) {
   let spaceRegex = /\s/g
   let nonAlphaRegex = /[^a-z]/gi
   str = str.replace(spaceRegex, '').replace(nonAlphaRegex, '').toLowerCase()
-  let strSet = new Set(str)
-  if (strSet.size !== Math.ceil(str.length / 2)) return false
-  else return true
+  let strHash = {}
+  for (let letter of str){
+      strHash[letter] ? strHash[letter]++ : strHash[letter] = 1
+  }
+  endingValues = Object.values(strHash)
+  let numOdd = 0
+  for (let item of endingValues){
+      if (item % 2 !== 0) numOdd++
+      if (numOdd > 1) return false
+  }
+  return true
 }
+
+
 
 console.group('palendromePermutation quick tests')
 console.log(palendromePermutation(yesP)) //true
@@ -25,4 +38,6 @@ console.log(palendromePermutation(emptyStr)) //-1
 console.log(palendromePermutation(noP)) // false
 console.log(palendromePermutation(space)) //true
 console.log(palendromePermutation(ignoreNonLet)) //true
+console.log(palendromePermutation(tooManyRepeats)) //false
+console.log(palendromePermutation(tooManyRepeatsTrue)) //true
 console.groupEnd()
